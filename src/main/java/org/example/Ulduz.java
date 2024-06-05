7f4ea7d7096385802e28108a425ebe1458251005
Qedir <qedirbabayev3@gmail.com>
2024-06-05 16:44:54 +0400

    ulduz new

--- src/main/java/org/example/Ulduz.java
+++ src/main/java/org/example/Ulduz.java
@@ -4,7 +4,7 @@ public class Ulduz {
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < 10-i; j++) {
                 System.out.print("*");
             }
             System.out.println();
