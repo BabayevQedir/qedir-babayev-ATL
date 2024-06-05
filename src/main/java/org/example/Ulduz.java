43c1648040329a47cea49ce499454bbec02f52a5
Qedir <qedirbabayev3@gmail.com>
2024-06-05 16:42:03 +0400

    ulduz

---
+++ src/main/java/org/example/Ulduz.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Ulduz {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
