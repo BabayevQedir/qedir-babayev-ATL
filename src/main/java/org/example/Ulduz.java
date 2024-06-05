75a07d3df4381f310e4526502c5bcc90ba02acea
Qedir <qedirbabayev3@gmail.com>
2024-06-05 17:48:42 +0400

    ulduzw

--- src/main/java/org/example/Ulduz.java
+++ src/main/java/org/example/Ulduz.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Ulduz {
     public static void main(String[] args) {
 
@@ -8,6 +10,28 @@ public class Ulduz {
                 System.out.print("*");
             }
             System.out.println();
+        }
+
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for (int i = 0; i <= a; i++) {
+            for (int j = 0; j < a-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <=i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
         }
     }
 }
